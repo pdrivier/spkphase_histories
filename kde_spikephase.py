13702faea94bbb17f8c468814b0b0ae6a07ee0e6
pdrivier <33138648+pdrivier@users.noreply.github.com>
2022-06-17 14:09:17 -0700

    new function uses arrays instead of df

--- kde_spikephase.py
+++ kde_spikephase.py
@@ -36,6 +36,32 @@ def kde_spikephase_estimator(df,n_splits,grid_size,bw_narrow,bw_wide,n_bw):
     bw = kde.bandwidth
     return pdf, kde, bw, x
 
+def kde_spikephase_estimator_forarray(phi_when_sp,n_splits,grid_size,bw_narrow,bw_wide,n_bw):
+
+
+    phase_grid = np.linspace(-np.pi, np.pi, grid_size)
+
+    grid = GridSearchCV(KernelDensity(kernel='gaussian'),
+                        {'bandwidth': np.linspace(bw_narrow,bw_wide,n_bw)},
+                        cv = n_splits)
+
+    #hacky way to get the kernel to treat phase circularly
+    cycle1 = phi_when_sp - 2*np.pi
+    cycle2 = phi_when_sp
+    cycle3 = phi_when_sp + 2*np.pi
+
+    fitting_sample = np.hstack((cycle1, cycle2, cycle3))
+    grid.fit(fitting_sample[:,None])
+
+    # print(grid.best_params_)
+
+    kde = grid.best_estimator_
+    pdf = np.exp(kde.score_samples(phase_grid[:,None]))
+    pdf = pdf * 3 #adjust for the extra data added on either side
+
+    bw = kde.bandwidth
+    return pdf, kde, bw
+
 def plot_kde(pdf,spkphase_hist, bw, phase_grid,ax=None):
     "Create plot of kde estimator over histogram of spikes by phase"
 
